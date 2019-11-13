f2f393b567f7b5111c5de9cd2bf818283a0d37d7
gauthier <gauthier.deville@gmail.com>
2019-11-13 09:43:17 +0100

    Ignore

--- DAG/Myfirstdag.py
+++ DAG/Myfirstdag.py
@@ -12,11 +12,7 @@ default_args = {
     'email_on_retry': True,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
-}
+ }
 
 dag = DAG('Run airflow check',
           default_args=default_args,
