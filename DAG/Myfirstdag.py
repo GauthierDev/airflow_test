5cf6568028696ff0f986f2ab156a94803b8cdc96
gauthier <gauthier.deville@gmail.com>
2019-11-13 08:28:42 +0100

    rename task

--- DAG/Myfirstdag.py
+++ DAG/Myfirstdag.py
@@ -20,19 +20,19 @@ default_args = {
 
 dag = DAG('Run airflow check',
           default_args=default_args,
-          shedule_interval=timedelta(minutes=5))
+          schedule_interval=timedelta(minutes=5))
 
-process_status_web = BashOperator(task_id='ps web server',
+process_status_web = BashOperator(task_id='ps.web.server',
                                   bash_command='ps -eaf | '
                                                'grep "master [airflow-webserver]"',
                                   dag=dag)
 
-process_status_worker = BashOperator(task_id='ps web server',
+process_status_worker = BashOperator(task_id='ps.worker.server',
                                      bash_command='ps -eaf | '
                                                   'grep "worker [airflow-webserver]"',
                                      dag=dag)
 
-process_status_scheduler = BashOperator(task_id='ps scheduler server',
+process_status_scheduler = BashOperator(task_id='ps.scheduler.server',
                                         bash_command='ps -eaf | grep scheduler',
                                         dag=dag)
 
